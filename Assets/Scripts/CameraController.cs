0bb56296551a9fe3c89ec49ba6282a8a657e0afe
TravisEntner <57158299+TravisEntner@users.noreply.github.com>
2019-12-17 20:05:18 -0500

    roll-a-ball

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+
 	private Vector3 offset;
 
+   
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+
 		offset = transform.position - player.transform.position;
 	}
 	
